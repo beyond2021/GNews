0f068538646d00ad487ab27f3c76fbe5ae2027fc
“KEEVIN <keevin.mitchell@gmail.com>
2015-07-22 21:37:28 -0400

    Pod file and othe dependencies added

---
+++ GNews/Pods/Target Support Files/Pods-GNews-MediaRSSParser/Pods-GNews-MediaRSSParser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GNews_MediaRSSParser : NSObject
+@end
+@implementation PodsDummy_Pods_GNews_MediaRSSParser
+@end
---
+++ GNews/Pods/Target Support Files/Pods-GNews/Pods-GNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GNews : NSObject
+@end
+@implementation PodsDummy_Pods_GNews
+@end
